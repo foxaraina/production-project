8fe2ba9168be5dba0993f6a0c2167435a86f9d56
dovzhikstasy <a.dovzhik@brl.ru>
2023-07-12 20:36:29 +0300

    test pull request ci

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -8,7 +8,7 @@ const MainPage: FC = () => {
         <div>
             <BugButton />
             {t('Main Page')}
-            <p className="test">{t('Основная страница сайта')}</p>
+            <p className="tes">{t('Основная страница сайта')}</p>
         </div>
     );
 };
